271912d984dd0424b96227c820602bae325ed853
gutolpn <gutolpn@gmail.com>
2019-08-21 11:22:39 -0300

    estrutura inicial

--- main.c
+++ main.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define COLUMNS 60
+#define ROWS 25
+
+void init(char matrix[ROWS][COLUMNS]){
+    int i,j;
+
+    for(i=0; i<ROWS; i++){
+        for(j=0; j<COLUMNS; j++){
+            matrix[i][j] = ' ';
+        }
+    }
+}
+
+void printMatrix(char matrix[ROWS][COLUMNS]){
+    int i,j;
+
+    for(i=0; i<ROWS; i++){
+        for(j=0; j<COLUMNS; j++){
+            printf("%c", matrix[i][j]);
+        }
+
+        printf("\n");
+    }
+}
 
 int main(){
-    
-    printf("Hello World!");
+    char matrix[ROWS][COLUMNS];
+    int posI, posJ;
+
+    //posicao inicial do personagem
+    posI = ROWS/2;
+    posJ = COLUMNS/2;
+    //inicializando matriz
+    init(matrix);
+
+    while(1){
+        system("cls");
+
+        matrix[posI][posJ] = '@';
+
+        printMatrix(matrix);
+
+        matrix[posI][posJ] = ' ';
+        
+        if(posJ < COLUMNS) posJ++;
+    }
+
+    system("pause");
 
     return 0;
 }
